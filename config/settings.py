89371dd95d9486bae2c6d79fde7cde346e046863
Setayesh Mohammadi <setayeshmbr26@gmail.com>
2023-04-24 18:10:54 +0330

    update requirements

--- config/settings.py
+++ config/settings.py
@@ -155,7 +155,8 @@ STATICFILES_DIRS = [
 ]
 
 # sending email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_USE_TLS = True
 EMAIL_PORT = config('EMAIL_PORT')
